67112e3623081fe50563977ce421bd3c716ae1a1
henriqueholtz <henrique_holtz@hotmail.com>
2022-04-05 07:28:11 -0300

    Add httpserver into cmd/server.go and Add curse.proto

--- cmd/server.go
+++ cmd/server.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/henriqueholtz/imersion-fullcycle-golang/model"
+	"net/http"
+)
+
+var courseList = model.NewCourses()
+
 func main() {
-	print("Hello!")
+	print("Hello! Server is running at 8888 port.")
+	http.HandleFunc("/courses", CourseListHanlder)
+	http.HandleFunc("/courses", CourseListHanlder)
+	http.ListenAndServe(":8888", nil)
+}
+
+func CourseListHanlder(w http.ResponseWriter, r *http.Request) {
+	courseJson, _ := json.Marshal(courseList)
+	w.Write([]byte(courseJson))
 }
---
+++ grpc/service/course.go
@@ -0,0 +1,9 @@
+package service
+
+//import "github.com/henriqueholtz/imersion-fullcycle-golang/grpc/pb"
+
+type CourseGrpcService struct {
+	//pb.Unim
+	//1h04 https://www.youtube.com/watch?v=ZB1Nl096l40&t=2161s
+	//Courses model
+}
